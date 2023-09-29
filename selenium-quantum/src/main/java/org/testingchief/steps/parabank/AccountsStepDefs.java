79477b40ae4c3bf921e86d21d3ab6a6b8e6127bd
skthetester <skthetester@gmail.com>
2023-09-28 20:59:48 -0400

    added stepdefs

--- selenium-quantum/src/main/java/org/testingchief/steps/parabank/AccountsStepDefs.java
+++ selenium-quantum/src/main/java/org/testingchief/steps/parabank/AccountsStepDefs.java
@@ -4,6 +4,7 @@ import com.github.javafaker.Faker;
 import com.qmetry.qaf.automation.step.QAFTestStep;
 import com.qmetry.qaf.automation.step.QAFTestStepProvider;
 import com.qmetry.qaf.automation.ui.webdriver.QAFExtendedWebElement;
+import cucumber.api.DataTable;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
@@ -21,7 +22,7 @@ public class AccountsStepDefs {
     }
 
     @QAFTestStep(description = "enter the required customer information{0}")
-    public void enter_the_required_customer_information(Object[] objArray) throws InterruptedException {
+    public void enter_the_required_customer_information(Object[] objArray) {
         Faker faker = new Faker();
         String password = "";
         for (Object obj : objArray) {
@@ -58,7 +59,7 @@ public class AccountsStepDefs {
     public void user_clicks_on_register_button() {
         QAFExtendedWebElement registerBtn = new QAFExtendedWebElement(By.xpath("//input[@value='Register']"));
         registerBtn.click();
-        
+
     }
 
     @Then("^the user is created successfully$")
@@ -69,5 +70,147 @@ public class AccountsStepDefs {
         QAFExtendedWebElement accountCreationMsg = new QAFExtendedWebElement(By.cssSelector("div[id='rightPanel'] p"));
         assertThat(accountCreationMsg.getText(), containsString("Your account was created successfully. You are now logged in."));
     }
+
+    @When("^user clicks on \"([^\"]*)\" link$")
+    public void user_clicks_on_link(String arg1) {
+
+
+    }
+
+    @Then("^the \"([^\"]*)\" page is displayed$")
+    public void the_page_is_displayed(String arg1) {
+
+
+    }
+
+    @Then("^the following account information is displayed$")
+    public void the_following_account_information_is_displayed(DataTable arg1) {
+
+
+    }
+
+
+
+    @Then("^the following account activity details are displayed$")
+    public void the_following_account_activity_details_are_displayed(DataTable arg1) {
+
+
+    }
+
+    @When("^user selects a All value from dropdown$")
+    public void user_selects_a_All_value_from_dropdown() {
+
+
+    }
+
+    @When("^user clicks on \"([^\"]*)\" button$")
+    public void user_clicks_on_button(String arg1) {
+
+
+    }
+
+    @When("^user selects a currentMonth value from dropdown$")
+    public void user_selects_a_currentMonth_value_from_dropdown() {
+
+
+    }
+
+    @When("^user selects a nextMonth value from dropdown$")
+    public void user_selects_a_nextMonth_value_from_dropdown() {
+
+
+    }
+
+    @Then("^\"([^\"]*)\" message is displayed$")
+    public void message_is_displayed(String arg1) {
+
+
+    }
+
+    @When("^user selects a Credit value from dropdown$")
+    public void user_selects_a_Credit_value_from_dropdown() {
+
+
+    }
+
+    @When("^user selects a Debit value from dropdown$")
+    public void user_selects_a_Debit_value_from_dropdown() {
+
+
+    }
+
+    @When("^user enters the transfer details$")
+    public void user_enters_the_transfer_details(DataTable arg1) {
+
+
+    }
+
+    @When("^user clicks on an account number$")
+    public void user_clicks_on_an_account_number(DataTable arg1) {
+
+
+    }
+
+
+    @When("^user enters the payee details$")
+    public void user_enters_the_payee_details(DataTable arg1) {
+
+
+    }
+
+    @When("^user selects an account$")
+    public void user_selects_an_account(DataTable arg1) {
+
+
+    }
+
+    @Then("^the transaction details are displayed$")
+    public void the_transaction_details_are_displayed() {
+
+
+    }
+
+    @When("^the user enters the loan information$")
+    public void the_user_enters_the_loan_information(DataTable arg1) {
+
+
+    }
+
+    @Then("^the loan details are displayed$")
+    public void the_loan_details_are_displayed(DataTable arg1) {
+
+
+    }
+
+    @Then("^the status is displayed as \"([^\"]*)\"$")
+    public void the_status_is_displayed_as(String arg1) {
+
+
+    }
+
+    @When("^user clicks on the new account created$")
+    public void user_clicks_on_the_new_account_created() {
+
+
+    }
+
+    @Then("^the account type is displayed as LOAN$")
+    public void the_account_type_is_displayed_as_LOAN() {
+
+
+    }
+
+    @Then("^\"([^\"]*)\" is displayed$")
+    public void is_displayed(String arg1) {
+
+
+    }
+
+    @When("^enter the required customer information$")
+    public void enter_the_required_customer_information(DataTable arg1) {
+
+
+    }
+
 
 }
